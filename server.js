18a7becffbe48bff9888e3c8b026309b5e82c544
Jason <jason1985@gmail.com>
2021-02-24 21:32:22 +0700

    ready to deploy

---
+++ server.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const express = require('express')
+const http = require('http')
+const app = express()
+const server = http.createServer(app)
+const socket = require('socket.io')
+const io = socket(server)
+const path = require('path')
+
+const users = {}
+
+io.on('connection', (socket) => {
+  if (!users[socket.id]) {
+    users[socket.id] = socket.id
+  }
+  socket.emit('yourID', socket.id)
+  io.sockets.emit('allUsers', users)
+  socket.on('disconnect', () => {
+    delete users[socket.id]
+  })
+
+  socket.on('callUser', (data) => {
+    io.to(data.userToCall).emit('hey', {
+      signal: data.signalData,
+      from: data.from,
+    })
+  })
+
+  socket.on('acceptCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal)
+  })
+})
+
+if (process.env.PROD) {
+  app.use(express.static(path.join(__dirname, './client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
+  })
+}
+
+const port = process.env.PORT || 8000
+
+server.listen(port, () => console.log(`server is running on port ${port}`))
